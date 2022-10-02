996c845168b18322c8b6c9195e511b395f030edb
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-10-02 09:15:48 -0400

    connected to mongoDB

---
+++ README.md
@@ -0,0 +1,3 @@
+## notes 
+- store 10 question obj from API in local storage? cookies? 
+- user obj: username, pass, scores, avg score 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express') 
+const mongoose = require('mongoose')
 const triviaRoutes = require('./routes/trivia')
 
 const app = express(); 
@@ -13,7 +14,16 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/trivia', triviaRoutes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`)
-})
+// db connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to DB and listening on port ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+
 
